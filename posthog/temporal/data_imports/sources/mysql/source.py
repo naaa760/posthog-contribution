f746ba7a78daa5e59e9f12de350f2fa3657b8a1b
naaa760 <neh6a683@gmail.com>
2025-12-02 13:58:11 +0530

    populate column names in MySQL

--- posthog/temporal/data_imports/sources/mysql/source.py
+++ posthog/temporal/data_imports/sources/mysql/source.py
@@ -127,6 +127,7 @@ class MySQLSource(SimpleSource[MySQLSourceConfig], SSHTunnelMixin, ValidateDatab
 
         for table_name, columns in db_schemas.items():
             column_info = [(col_name, col_type) for col_name, col_type in columns]
+            column_names = [col_name for col_name, _ in column_info]
 
             incremental_field_tuples = filter_mysql_incremental_fields(column_info)
             incremental_fields: list[IncrementalField] = [
@@ -145,6 +146,7 @@ class MySQLSource(SimpleSource[MySQLSourceConfig], SSHTunnelMixin, ValidateDatab
                     supports_incremental=len(incremental_fields) > 0,
                     supports_append=len(incremental_fields) > 0,
                     incremental_fields=incremental_fields,
+                    columns=column_names,
                 )
             )
 
